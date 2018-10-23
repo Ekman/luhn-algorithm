f967349de3177dc96490eb75cb8c97ae62beafce
Niklas Ekman <nikl.ekman@gmail.com>
2018-10-23 17:42:49 +0200

    Remove phpunit dependency

---
+++ run-phpunit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#
+# Runs code standard fixer without having the need to specify it as a
+# Composer dependency.
+#
+
+composer require --no-interaction phpunit/phpunit:^6.0
+
+vendor/bin/phpunit --configuration phpunit.xml
+
+composer remove --no-interaction phpunit/phpunit
