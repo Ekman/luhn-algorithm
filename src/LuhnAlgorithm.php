a7d281ed0186aabcff719422ba5265ef21b5a34e
Niklas Ekman <nikl.ekman@gmail.com>
2018-11-23 09:07:42 +0100

    Update copyright notice (#17)
    * Remove unnesecary code
    
    * Ignore private constructor in code coverage
    
    * Fix tests
    
    * Fix tests
    
    * Update copyright notice

--- src/LuhnAlgorithm.php
+++ src/LuhnAlgorithm.php
@@ -3,7 +3,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2014 Niklas Ekman
+ * Copyright (c) 2018 Niklas Ekman
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -44,11 +44,9 @@ class LuhnAlgorithm implements LuhnAlgorithmInterface
             throw new \InvalidArgumentException("Check digit is null.");
         }
 
-        $checksum = $this->calcChecksum($number);
-        $sum = $checksum + $number->getCheckDigit();
+        $checksum = $this->calcChecksum($number) + $number->getCheckDigit();
 
-        // If the checksum is divisible by 10 it is valid.
-        return ($sum % 10) === 0;
+        return ($checksum % 10) === 0;
     }
 
     /**
@@ -61,7 +59,6 @@ class LuhnAlgorithm implements LuhnAlgorithmInterface
         // Get the last digit of the checksum.
         $checkDigit = $checksum % 10;
 
-        // If the check digit is not 0, then subtract the value from 10.
         return $checkDigit === 0
             ? $checkDigit
             : 10 - $checkDigit;
@@ -72,14 +69,13 @@ class LuhnAlgorithm implements LuhnAlgorithmInterface
      */
     public function calcChecksum(NumberInterface $number): int
     {
-        $number = (string) $number->getNumber();
-        // Need to account for the check digit.
-        $nDigits = strlen($number) + 1;
-        $parity = $nDigits % 2;
+        $nDigits = strlen($number->getNumber());
+        // Need to account for check digit
+        $parity = ($nDigits + 1) % 2;
         $checksum = 0;
 
-        for ($i = 0; $i < $nDigits - 1; $i++) {
-            $digit = (int) $number[$i];
+        for ($i = 0; $i < $nDigits; $i++) {
+            $digit = (int) $number->getNumber()[$i];
 
             // Every other digit, starting from the rightmost,
             // shall be doubled.
