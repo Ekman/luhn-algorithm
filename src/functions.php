e841a4537e4b0c8d1a681a9a61572baf420149fe
CircleCI <circleci@Ekman.example>
2021-01-12 11:17:17 +0000

    [CircleCI] Fix coding standards

--- src/functions.php
+++ src/functions.php
@@ -33,7 +33,7 @@ namespace Nekman\LuhnAlgorithm\Functions;
  * @return bool true if the number consists only of numbers, false otherwise
  * @internal
  */
-function string_is_numeric(         string $possibleNumber): bool
+function string_is_numeric(string $possibleNumber): bool
 {
     return preg_match("/^\d+(\.\d+)?$/", $possibleNumber) === 1;
 }
